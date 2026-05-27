d86f48a81b3dc05c0a1029b457decdc21cdb2526
Vinay Rawoorkar <vinayrawoorkar328@gmail.com>
2026-05-28 04:48:20 +0530

    fixed backend urls

--- backend/app.py
+++ backend/app.py
@@ -183,8 +183,4 @@ def delete_product(id):
 # =========================================
 
 if __name__ == "__main__":
-
-    app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 5000))
-)
+    app.run(host="0.0.0.0", port=5000)
